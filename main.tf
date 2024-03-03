f523c98421d3def954a569ff784865c4aa2e4d9d
damar121 <93617182+damar121@users.noreply.github.com>
2024-03-03 15:13:30 +0200

    Fix Typo in main

--- main.tf
+++ main.tf
@@ -25,20 +25,20 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld2"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.1"
-  name = "blog_new"
+  name    = "blog_new"
 
-  vpc_id      = data.aws_vpc.default.id
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  vpc_id              = data.aws_vpc.default.id
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all]
+  egress_rules        = ["all-all"]
   engress_cidr_blocks = ["0.0.0.0/0"]
 
 }
